49bf59043973424f193c3d68bb3c5feb243348ec
turb <turb@pisscake.com>
2015-10-21 23:33:08 -0700

    function as local variable to prevent conflict

--- brackets/frontend.js
+++ brackets/frontend.js
@@ -20,42 +20,42 @@
 				change.innerHTML = message['content'];
 				break;
 			case 'eval':
-				eval(message['js'];);
+				eval(message['js']);
 				break;
 		}
 	};
+
+	var  setHighlighted = function(selector){
+		existingSelectors = document.querySelectorAll('.brackets-currently-selected-highlight');
+		for(var i = 0, len = existingSelectors.length; i < len; i++){
+			existingSelectors[i].parentElement.removeChild(existingSelectors[i]);
+		}
+
+		toHighlight = document.querySelectorAll(message['selector']);
+		for(var i = 0, len = toHighlight.length; i < len; i++){
+			var newHighlight = document.createElement('div');
+			newHighlight.className = 'brackets-currently-selected-highlight';
+
+			var box = toHighlight[i].getBoundingClientRect();
+
+			var body = document.body;
+			var docEl = document.documentElement;
+
+			var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+			var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+			var clientTop = docEl.clientTop || body.clientTop || 0;
+			var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+			var top  = box.top +  scrollTop - clientTop;
+			var left = box.left + scrollLeft - clientLeft;
+
+			newHighlight.style.top = top;
+			newHighlight.style.left = left;
+			newHighlight.style.width = box.right - box.left;
+			newHighlight.style.height = box.bottom - box.top;
+
+			document.body.appendChild(newHighlight);
+		}
+	}
 })();
-
-function setHighlighted(selector){
-	existingSelectors = document.querySelectorAll('.brackets-currently-selected-highlight');
-	for(var i = 0, len = existingSelectors.length; i < len; i++){
-		existingSelectors[i].parentElement.removeChild(existingSelectors[i]);
-	}
-
-	toHighlight = document.querySelectorAll(message['selector']);
-	for(var i = 0, len = toHighlight.length; i < len; i++){
-		var newHighlight = document.createElement('div');
-		newHighlight.className = 'brackets-currently-selected-highlight';
-
-		var box = toHighlight[i].getBoundingClientRect();
-
-		var body = document.body;
-		var docEl = document.documentElement;
-
-		var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-		var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-
-		var clientTop = docEl.clientTop || body.clientTop || 0;
-		var clientLeft = docEl.clientLeft || body.clientLeft || 0;
-
-		var top  = box.top +  scrollTop - clientTop;
-		var left = box.left + scrollLeft - clientLeft;
-
-		newHighlight.style.top = top;
-		newHighlight.style.left = left;
-		newHighlight.style.width = box.right - box.left;
-		newHighlight.style.height = box.bottom - box.top;
-
-		document.body.appendChild(newHighlight);
-	}
-}
