537feb870fc0b96f0befda972c2d05fb4cd1cc9d
turb <turb@pisscake.com>
2015-12-09 20:02:48 -0800

    still working on the parser

--- brackets/htmlfile.js
+++ brackets/htmlfile.js
@@ -68,24 +68,23 @@ HtmlFile.prototype.createElementPositions = function(){
 				return null;
 			}
 		},
-		forwardUntil: function(line, column, searchChars, callback){
+		forwardUntil: function(line, column, searchChars){
 			if(searchChars.constructor !== Array){
 				searchChars = searchChars.split('');
 			}
 
-			passedOver = '';
+			var passedOver = '';
+			var curChar = '';
 
 			while((curChar = this.charAt(line, column)) !== null){
 				passedOver += curChar;
 
 				if(searchChars.indexOf(curChar) > -1){
-					if(callback !== undefined){
-						callback(line, column, passedOver);
-						break;
-					}else{
-						return [line, column, data];
-					}
-
+					return {
+						line: line,
+						column: column,
+						data: passedOver
+					};
 				}
 
 				column++;
@@ -106,7 +105,51 @@ HtmlFile.prototype.createElementPositions = function(){
 				return null;
 			}
 		},
-		parse: function(state){
+		parse: function(){
+			var tags = [];
+			var parsedTag = this.parseNext();
+
+			while(parsedTag !== null){
+				tags.push(parsedTag);
+				parsedTag = this.parseNext({
+					line: parsedTag.end[0],
+					column: parsedTag.end[1]
+				});
+			}
+
+			return tags;
+		},
+		parseTag: function(line, column){
+			//go the the start of a tag
+			var tagStart = this.forwardUntil(line, column, '<');
+
+			//if tatStart is null, then there is his (hopefully) not another
+			//tag bacause eof was reached before anything was found
+			if(tagStart == null){
+				return null;
+			}
+
+			//an now to the end of the tag
+			var tagEnd = this.forwardUntil(tagStart.line, tagStart.column, '>');
+			if(tagEnd == null){
+				return null;
+			}
+
+			//if we go to this point, it's (probably) a tag and thus
+			//we can (probably) safely create a tag object
+			var tag = {
+				text: tagEnd.data,
+				name: this.tagName(tagEnd.data),
+				closing: tagEnd.data.startsWith('</'),
+				selfclosing: false,
+				start: [tagStart.line, tagStart.column],
+				end: [tagEnd.line, tagEnd.column],
+				children: []
+			};
+
+			return tag;
+		},
+		parseNext: function(state){
 			if(state == undefined){
 				state = {
 					line: 0,
@@ -114,44 +157,58 @@ HtmlFile.prototype.createElementPositions = function(){
 				};
 			}
 
-			var self = this;
+			if((state.tag = this.parseTag(state.line, state.column)) == null){
+				return null;
+			}
 
-			//go the the start of a tag
-			this.forwardUntil(state.line, state.column, '<', function(line, column, data){
-				//or if it's a comment
-				if(data.startsWith('<!--')){
-				}else if(data.startsWith('<script')){
-				}else if(data.startsWith('<style')){
-				}
-				state.line = line;
-				state.column = column;
+			//if this tag closes, just return it
+			if(state.tag.closing){
+				return state.tag;
+			}
 
-				self.forwardUntil(state.line, state.column, '>', function(line, column, data){
-					console.log(
-						state.line + ':' + state.column
-						+ '-'
-						+ line + ':' + column + '\t' + data);
-					//check it this is a closing tag
-					if(data.startsWith('</')){
-					}
-
-					state.tag = {
-						text: data,
-						start: [state.line, state.column],
-						end: [line,column],
-					}
-					self.parse({
-						line: line,
-						column: column
-					});
-				});
+			//otherwise, find the next tag starting from the last tag's
+			//ending point
+			nextTag = this.parseNext({
+				line: state.tag.end[0],
+				column: state.tag.end[1]
 			});
+
+			//if there's no next tag, return this frame's tag
+			if(nextTag == null){
+				return state.tag;
+			}
+
+			//if the next tag is closing
+			if(nextTag.closing){
+				//see if it closes this tag
+				if(state.tag.name == nextTag.name){
+					//and if so, change this tags end to where it's closing
+					//tag (the next tag) ends, and return this
+					state.tag.end = nextTag.end;
+					return state.tag;
+				}else{
+					//if the next tag is closing, but does not close this tag,
+					//this is (probably) a self closing tag
+					//state.tag.children.push(nextTag);
+					state.tag.selfclosing = true;
+					nextTag.children.push(state.tag);
+					return nextTag;
+				}
+			}else{
+				state.tag.children.push(nextTag);
+			}
+
+			return state.tag;
+		},
+		tagName(tagStr){
+			return tagStr.match(/<\/?\s*([a-zA-Z0-9]+).*>/)[1];
 		}
 	}
 
 	//start parsing
 	console.time('parsing');
-	parser.parse();
+	console.log(JSON.stringify(parser.parse(), null, 2));
+	//console.log(parser.parse());
 	console.timeEnd('parsing');
 }
 
