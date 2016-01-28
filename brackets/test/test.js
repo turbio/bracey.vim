d95372f0050579aaa8b1f73a6ff1652d667df6b9
turb <turb@pisscake.com>
2016-01-28 09:05:33 -0800

    quite a few test cases for the tagfromposition function

--- brackets/test/test.js
+++ brackets/test/test.js
@@ -41,5 +41,20 @@ describe('htmlfile', function(){
 			expect(this.file.tagFromPosition(10000, 0)).to.be.null;
 			expect(this.file.tagFromPosition(0, 10000)).to.be.null;
 		});
+		[
+			{line: 1, column: 0, expected: {name: 'html', index: 1}},
+			{line: 3, column: 0, expected: {name: 'head', index: 2}},
+			{line: 3, column: 1, expected: {name: 'title', index: 3}},
+			{line: 21, column: 4, expected: {name: 'body', index: 6}},
+			{line: 30, column: 1, expected: {name: 'ol', index: 13}},
+			{line: 52, column: 1, expected: {name: 'tr', index: 29}},
+			{line: 53, column: 1, expected: {name: 'table', index: 23}},
+		].forEach(function(test){
+			it('finds the correct element on ' + test.line + ':' + test.column, function(){
+				var elem = this.file.tagFromPosition(test.line, test.column);
+				elem.should.have.property('name', test.expected.name);
+				elem.should.have.property('index', test.expected.index);
+			});
+		}, this);
 	});
 });
