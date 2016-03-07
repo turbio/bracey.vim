4c0d18035656acd4df63703bbb7ec69db910044e
turbio <turbio@masonclayton.com>
2016-03-07 00:16:46 -0500

    fix unit test send and receive

--- brackets/test/server.js
+++ brackets/test/server.js
@@ -15,14 +15,27 @@ function send(msg){
 
 	var request = http.request(options, function(response){
 		expect(response.statusCode).to.equal(200);
+		response.on('data', function(data){});
+		response.on('end', function(){});
 	});
+
+	request.on('error', function(e){
+		e.should.be.null;
+	});
+
 	request.write(msg);
 	request.end();
 }
 
 var callbacks = [];
+var responses = [];
+
 function recieve(callback){
-	callbacks.push(callback);
+	if(responses.length > 0){
+		callback(responses.pop());
+	}else{
+		callbacks.push(callback);
+	}
 }
 
 describe('server', function(){
@@ -38,6 +51,8 @@ describe('server', function(){
 			connection.on('message', function(msg){
 				if(callbacks.length > 0){
 					callbacks.pop()(JSON.parse(msg.utf8Data));
+				}else{
+					responses.push(JSON.parse(msg.utf8Data));
 				}
 			});
 			done();
@@ -54,12 +69,14 @@ describe('server', function(){
 		server.stop();
 	});
 
-	it('responds with pong when send ping', function(done){
-		recieve(function(msg){
-			msg.should.deep.equal({"command": "pong"});
-			done();
+	describe('ping', function(){
+		it('responds with pong when send ping', function(done){
+			send('ping');
+			recieve(function(msg){
+				msg.should.deep.equal({"command": "pong"});
+				done();
+			});
 		});
-		send('ping');
 	});
 
 	describe('position', function(){
@@ -70,9 +87,10 @@ describe('server', function(){
 			{command: 'p:22:5', expected: 6},
 			{command: 'p:31:2', expected: 13},
 			{command: 'p:53:2', expected: 29},
-			{command: 'p:54:2', expected: 23},
+			{command: 'p:54:2', expected: 23}
 		].forEach(function(test){
 			it('correctly responds to the position command "' + test.command + '"', function(done){
+				send(test.command);
 				recieve(function(msg){
 					msg.should.deep.equal({
 						"command": "select",
@@ -80,12 +98,7 @@ describe('server', function(){
 					});
 					done();
 				});
-				send(test.command);
 			});
 		}, this);
-	});
-
-	describe('set contents', function(){
-		it('correctly responds to buffer changes');
 	});
 });
