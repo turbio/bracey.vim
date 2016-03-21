8046d921fdaff6129045789a79c8324d0b20f7b7
turb <turb@pisscake.com>
2016-03-21 12:31:21 -0700

    brackets server command line arguments

--- brackets/brackets.js
+++ brackets/brackets.js
@@ -1,5 +1,47 @@
-var port = 1337;
+var args = [
+	{
+		short: 'p',
+		long: 'port',
+		value: true
+	},
+	{
+		short: 'a',
+		long: 'address',
+		value: true
+	},
+	{
+		short: 'r',
+		long: 'remote-connect',
+		value: false
+	},
+	{
+		short: 'v',
+		long: 'remote-vim',
+		value: false
+	},
+];
+
+var settings = {};
+
+for(var i = 2; i < process.argv.length; i++){
+	var arg = process.argv[i];
+	if(!arg.startsWith('-')){
+		continue;
+	}
+
+	if(arg.startsWith('--')){
+		arg = arg.substr(2);
+		arg = args.find(function(f){ return f.long == arg; });
+	}else{
+		arg = arg.substr(1);
+		arg = args.find(function(f){ return f.short == arg; });
+	}
+
+	if(arg){
+		settings[arg.long] = (arg.value) ? process.argv[i + 1] : true
+	}
+};
 
 var server = require("./server.js");
 server = new server();
-server.start(port);
+server.start(settings.port || 13378, settings.address || '127.0.0.1');
